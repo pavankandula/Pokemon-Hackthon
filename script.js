766ac92aad69d73293fb92a0b0fa4cdf3bb5e952
pavankandula <pavansrivenkatasai@gmail.com>
2021-07-23 11:48:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
 getPokemons();
 
 //Get data from pokemon-mockapi
-async function getPokemons(){
-    const data = await fetch("https://pokeapi.co/api/v2/pokemon/",{         // fetch the data from api
-        method:"GET"
-    })
-    const pokemonData = await data.json(); // converting the data into json format             
-    var dataX = pokemonData.results;
+async function getPokemons() {
+  const data = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151", {
+    // fetch the data from api
+    method: "GET"
+  });
+  const pokemonData = await data.json(); // converting the data into json format
+  var dataX = pokemonData.results;
 
-    //console.log(dataX);
-    const noOfPages = Math.ceil(dataX.length / 5);
-    const pagination = document.querySelector(".pagination");
-    for (let i = 1; i <= noOfPages; i++) {
+  //console.log(dataX);
+  const noOfPages = Math.ceil(dataX.length / 10);
+  const pagination = document.querySelector(".pagination");
+  for (let i = 1; i <= noOfPages; i++) {
     const page = document.createElement("button");
     page.innerText = i;
     pagination.append(page);
     // page
-    
+
     page.onclick = function () {
-        var z = page.innerText;
-        z--;
-        const pageUsers = dataX.slice((z*5),(z*5)+5);
-        document.querySelector(".pokemonContent").innerHTML = "";
-        Split(pageUsers);
+      var z = page.innerText;
+      z--;
+      const pageUsers = dataX.slice(z * 10, z * 10 + 10);
+      document.querySelector(".pokemonContent").innerHTML = "";
+      Split(pageUsers);
     };
-    
-    }
-    const firstTenUsers = dataX.slice(0, 5);
-    // console.log(firstTenUsers);
-    // console.log("No of Pokemons are ", dataX.length);
-    Split(firstTenUsers);
-    function Split(firstTenUsers){
-        firstTenUsers.forEach((pokemon) => {     // getting the url of each object present in the data
-            localStorage.setItem("url",pokemon.url);   
-            getInnerData();     // calling InnerData function as it is array of objects
-        });
-    }
-    
+  }
+  const firstTenUsers = dataX.slice(0, 10);
+  // console.log(firstTenUsers);
+  // console.log("No of Pokemons are ", dataX.length);
+  Split(firstTenUsers);
+  function Split(firstTenUsers) {
+    firstTenUsers.forEach((pokemon) => {
+      // getting the url of each object present in the data
+      localStorage.setItem("url", pokemon.url);
+      getInnerData(); // calling InnerData function as it is array of objects
+    });
+  }
 }
 
-async function getInnerData(){
-    let url = localStorage.getItem("url");
-    let innerData = await fetch(url,{   // fetching the innnerData present in each url 
-        method:"GET"                    // to get the details of each pokemon
-    })
-    let pokeInnerData = await innerData.json();     //converting the innerdata int json format
-    // console.log(pokeInnerData);
-    //  console.log(pokeInnerData.moves[0].move.name);
-    //console.log(pokeInnerData);
-    pokemonContent(pokeInnerData)   //calling the pokemonContnent by passing innerData as parameter
+async function getInnerData() {
+  let url = localStorage.getItem("url");
+  let innerData = await fetch(url, {
+    // fetching the innnerData present in each url
+    method: "GET" // to get the details of each pokemon
+  });
+  let pokeInnerData = await innerData.json(); //converting the innerdata int json format
+  // console.log(pokeInnerData);
+  //  console.log(pokeInnerData.moves[0].move.name);
+  //console.log(pokeInnerData);
+  pokemonContent(pokeInnerData); //calling the pokemonContnent by passing innerData as parameter
 }
 
-function pokemonContent(pokeInnerData){
-    let innerContent = document.querySelector(".pokemonContent");   
-    let abilityName=[];
-    let moveName=[];
-    let {id}=pokeInnerData
-    console.log(id);
-    pokeInnerData.abilities.forEach((x)=>abilityName.push(x.ability.name));     // get each ability of every pokemon
-    pokeInnerData.moves.forEach((x)=>moveName.push(x.move.name));               // get each move of every pokemon
-    let weight = pokeInnerData.weight;                                          // get weight of every pokemon
-    //console.log(weight)
-    let uniquepokemon = document.createElement("div");          // alloting the data as per the requirement in UI using jQuery
-    uniquepokemon.innerHTML=`                                   
+function pokemonContent(pokeInnerData) {
+  let innerContent = document.querySelector(".pokemonContent");
+  let abilityName = [];
+  let moveName = [];
+  let { id } = pokeInnerData;
+  console.log(id);
+  pokeInnerData.abilities.forEach((x) => abilityName.push(x.ability.name)); // get each ability of every pokemon
+  pokeInnerData.moves.forEach((x) => moveName.push(x.move.name)); // get each move of every pokemon
+  let weight = pokeInnerData.weight; // get weight of every pokemon
+  //console.log(weight)
+  let uniquepokemon = document.createElement("div"); // alloting the data as per the requirement in UI using jQuery
+  uniquepokemon.innerHTML = `                                   
     <div class="innerdata card m-5">
         <div class="card-body response">
             <div class="img-div">
@@ -77,9 +78,7 @@ function pokemonContent(pokeInnerData){
                 <p class="weight card-text font-weight-bold"><span class="inner-headers  text-light">Weight:</span><span class="weightData ml-3">${weight}</span></p>
             </div>
         </div>    
-    </div>`
-    length--;
-    innerContent.append(uniquepokemon);
+    </div>`;
+  length--;
+  innerContent.append(uniquepokemon);
 }
-
-
